d763edcc8605a1b2429507f6e49335a2ce5f0f67
Franco Canales <fcanales18@gmail.com>
2023-03-06 13:54:46 -0500

    updated api test

---
+++ StatusSolution/Status/StatusMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Status
+{
+    public record StatusMessage(Guid Id, string Message, DateTimeOffset When)
+    {
+
+    }
+}
