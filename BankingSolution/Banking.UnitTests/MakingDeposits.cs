4f365853da42eb2ac33fa3f657783108efd4c3d7
Franco Canales <fcanales18@gmail.com>
2023-03-07 16:09:26 -0500

    more testing

---
+++ BankingSolution/Banking.UnitTests/MakingDeposits.cs
@@ -0,0 +1,26 @@
+﻿using Banking.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.UnitTests;
+
+public class MakingDeposits
+{
+    [Fact]
+    public void DeposistsIncreasesTheBalance()
+    {
+        //Given
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance(); //Query (Func)
+        var amountToDeposit = 100M;
+        //When
+        account.Deposit(amountToDeposit); //Command (Action)
+        //Then
+        Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+
+    }
+}
+
---
+++ BankingSolution/Banking.UnitTests/NewAccounts.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.UnitTests;
+public class NewAccounts
+{
+  
+    [Fact]
+    public void NewAccountHasCorrectOpeningBalance()
+    {
+        //"Write the code you Wish you had"
+
+        //Given
+        //Type identifier = initializer
+        BankAccount account = new BankAccount();
+        //When
+        decimal balance = account.GetBalance();
+        //Then
+        Assert.Equal(5000, balance);
+    }
+}
