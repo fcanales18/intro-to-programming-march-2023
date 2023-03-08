6a373bbddee0c1c4ceb1136441fb49f585e65619
Franco Canales <fcanales18@gmail.com>
2023-03-08 15:59:04 -0500

    end of string calculator tdd kata 1 finished up to 3

---
+++ stringcalculator-wed8/StringCalculator.cs
@@ -0,0 +1,52 @@
+﻿
+using System.ComponentModel;
+
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    //What was done as a group up to #4
+    /*
+    public int Add(string numbers)
+    {
+        if (numbers == "") return 0;
+        var delimiter = ",";
+        if (numbers.StartsWith("//")) 
+        {
+            delimiter = numbers[2].ToString();
+            numbers = numbers.Substring(4);
+        } 
+        var sum = 0;
+        numbers = numbers.Replace("\n", ",");
+        var numberList = numbers.Split(',');
+        foreach(var number in numberList)
+        {
+            sum += int.Parse(number);
+        }
+        return sum;
+    }
+    */
+
+    // Instructor's example
+    public int Add(string numbers)
+    {
+        if (numbers == "") { return 0; }
+        var allowedDelimeters = new List<char> { ',', '\n' };
+
+        /*
+        var total = 0;
+
+        foreach(var n in numbers.Split(allowedDelimeters.ToArray()))
+        {
+            total += int.Parse(n);
+        }
+        return total;
+        */
+
+        return numbers.Split(allowedDelimeters.ToArray()) // { "1", "2", "3")
+            .Select(int.Parse) // Map [1,2,3]
+            .Sum(); // 6
+    }
+
+}
---
+++ stringcalculator-wed8/StringCalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    StringCalculator _calculator = new StringCalculator();
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+
+        var result = _calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("4",4)]
+    [InlineData("5",5)]
+    [InlineData("1242",1242)]
+    public void SingleDigits(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("4,5", 9)]
+    [InlineData("12,340", 352)]
+    public void TwoDigits(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    /*
+    [Theory]
+    [InlineData("4,5, 45,60", 114)]
+    [InlineData("12,340, 1", 353)]
+    public void MultipleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+    */
+
+    [Theory]
+    [InlineData("1\n2",3)]
+    [InlineData("1\n10", 11)]
+    [InlineData("99\n5", 104)]
+    public void NewLineSplitters(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    /*
+    [Fact]
+    public void DifferentDelimitters()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//;\n1;2");
+
+        Assert.Equal(3, result);
+    }
+    */
+}
