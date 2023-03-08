b06259c7104cfdf923830840c0f920a305dea1f8
Franco Canales <fcanales18@gmail.com>
2023-03-08 11:49:26 -0500

    Finished with flag 1. Gold Bonus

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -1,28 +1,31 @@
-﻿namespace Banking.Domain
-{ 
-    public class BankAccount
+﻿namespace Banking.Domain;
+
+public enum BankAccountType { Standard, Gold }
+
+public class BankAccount
+{
+    public BankAccountType AccountType = BankAccountType.Standard;
+    private decimal _balance = 5000;
+    public void Deposit(decimal amountToDeposit)
     {
-        private decimal _balance = 5000;
-        public void Deposit(decimal amountToDeposit)
-        {
-            _balance = _balance + amountToDeposit;
-        }
+        var bonus = AccountType == BankAccountType.Gold ? amountToDeposit * .10M : 0;
+        _balance += amountToDeposit + bonus;
+    }
 
-        public decimal GetBalance()
-        {
-            return _balance;
-        }
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
 
-        public void Withdraw(decimal amountToWithdraw)
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        if (amountToWithdraw > _balance)
         {
-            if(amountToWithdraw > _balance)
-            {
-                throw new OverdraftException();
-            } 
-            else
-            {
-                _balance -= amountToWithdraw;
-            }
+            throw new OverdraftException();
+        }
+        else
+        {
+            _balance -= amountToWithdraw;
         }
     }
 }
--- BankingSolution/Banking.UnitTests/GoldCustomerDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerDeposits.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,13 @@ public class GoldCustomerDeposits
     [Fact]
     public void GoldCustomersGetABonusOnDeposits()
     {
-        Assert.True(false);
+        var account = new BankAccount();
+        account.AccountType = BankAccountType.Gold;
+        var amountToDesposit = 100M;
+        var openingBalance = account.GetBalance();
+
+        account.Deposit(amountToDesposit);
+
+        Assert.Equal(amountToDesposit + 10M + openingBalance, account.GetBalance());
     }
 }
