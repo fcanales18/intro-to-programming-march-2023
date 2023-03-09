d7a5ece8cc01b0520cb7104324922a5e873c3a02
Franco Canales <fcanales18@gmail.com>
2023-03-09 12:41:15 -0500

    before lunch 3/9 - bank account stuff

---
+++ BankingSolution/Banking.UnitTests/BankAccountUsesBonusCalculator.cs
@@ -0,0 +1,18 @@
+﻿using Banking.Domain;
+using Banking.UnitTests.TestDoubles;
+
+
+namespace Banking.UnitTests;
+
+public class BankAccountUsesBonusesCalculator
+{
+    [Fact()]
+    public void IntegratesWithBonusCalculator()
+    {
+        var bankAccount = new BankAccount(new StubbedBonusCalculator());
+
+        bankAccount.Deposit(212.83M);
+
+        Assert.Equal(5000M + 212.83M + 12M, bankAccount.GetBalance());
+    }
+}
