8d3d08f093637658e55d59506e476978664a7866
Franco Canales <fcanales18@gmail.com>
2023-03-09 14:44:05 -0500

    Working version of first time using Alba api integration test

---
+++ OnCallDeveloperSolution/OnCallDeveloperApi/Controllers/OnCallDeveloperController.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OnCallDeveloperApi.Controllers;
+
+public class OnCallDeveloperController : ControllerBase
+{
+    //GET /oncalldeveloper -> 200 Ok
+    [HttpGet("/oncalldeveloper")]
+    public ActionResult GetOnCallDeveloper()
+    {
+        return Ok(); // 200 status code
+    }
+}
