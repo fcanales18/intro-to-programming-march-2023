e30f086f146d886fbd4e11c0a0cf1c9a86ddf466
Franco Canales <fcanales18@gmail.com>
2023-03-10 15:51:39 -0500

    3/10 EOD Friday With Collections tests

---
+++ CSharpSyntaxSolution/CSharpSyntax/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace CSharpSyntax;
+
+public class BankAccount
+{
+    private string _accountNumber = string.Empty;
+    private string _firstName = string.Empty;
+
+    internal BankAccount(string accountNumber, string firstName)
+    {
+        _accountNumber = accountNumber;
+        FirstName = firstName;
+    }
+
+    //FirstName read only
+    public string FirstName
+    {
+        get { return _firstName; }
+        private set { _firstName = value; }
+    }
+
+    public string LastName { get; private set; } = string.Empty;
+    public string GetAccountNumber()
+    {
+        return _accountNumber;
+    }
+
+}
+
+
+//Class that does some work fo rme
+//Services
+
+public class AccountManager
+{
+    public BankAccount GetAccountById(string accountNumber)
+    {
+        //go find in the database
+        var account = new BankAccount(accountNumber, "Jeff");
+        return account;
+    }
+
+    public void DoDeposit(BankAccount bankAccount, decimal amount)
+    {
+
+    }
+}
