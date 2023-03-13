c3b3ebde9d03e5503f79815ab5e98e39cc50ae1e
Franco Canales <fcanales18@gmail.com>
2023-03-13 12:33:43 -0400

    starting angular. before lunch 3/13

---
+++ frontend/src/app/components/masthead/masthead.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masthead',
+  templateUrl: './masthead.component.html',
+  styleUrls: ['./masthead.component.css']
+})
+export class MastheadComponent {
+}
