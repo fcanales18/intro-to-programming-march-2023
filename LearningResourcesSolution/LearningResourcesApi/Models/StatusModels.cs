a27e7661a792348a32e2900a3aec6dabdf12de46
Franco Canales <fcanales18@gmail.com>
2023-03-17 12:03:25 -0400

    Done with oncall developer api. Before the talk about Docker

---
+++ LearningResourcesSolution/LearningResourcesApi/Models/StatusModels.cs
@@ -0,0 +1,6 @@
+﻿namespace LearningResourcesApi.Models;
+
+//Status is always "Normal", "Degraded", "OnFire"
+public enum StatusLevel { Normal, Degraded, OnFire }
+public record StatusResponse(StatusLevel status, DateTime lastOutage, List<DateTime> upcomingOutages, StatusHelpInfo ForHelp); 
+public record StatusHelpInfo(string Contact, Dictionary<string, string> contactInfo);
--- LearningResourcesSolution/LearningResourcesApi/Using.cs
+++ LearningResourcesSolution/LearningResourcesApi/Using.cs
@@ -2,4 +2,5 @@
 global using LearningResourcesApi.Models;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
+global using LearningResourcesApi.Domain;
 
